e7d87cf6843eb195e8496923fcb3111e548da82f
Salvi Pascual <salvi.pascual@gmail.com>
2019-08-08 16:32:07 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # WEB2000_MusicAlbum
 Boilerplate to make a music album using Boostrap and jQuery
+
+Let's create a website to showcase Music Albums and play songs.
+
+A new-age music studio wants to create a website to showcase their singers. They offer you a two-pages boilerplate and ask you to use it as a reference to implement the site using Boostrap and jQuery, by following the next steps:
+
+1) Clone the repo to your local computer and include Boostrap and jQuery to both HTML pages.
+
+2) Replace the images by the HTML/CSS code that produces the same or similar results. Hardcode all the texts and images for now.
+
+3) Update the page "home.html" to list the albums stored in the file "albums.js". There are three albums in the file, but if they add another one it should show withouth modifiying the HTML file. Once you click an album, it should take you to "album.html?id=XXX", where XXX is the album id.
+
+4) Update the page "album.html", so when it receives a request of the type "album.html?id=XXX", displays the album information from "albums.js". The page play a song if the "play" button is clicked. 
+
---
+++ js/albums.js
@@ -0,0 +1,220 @@
+var albums = [
+	{
+		id: 1,
+		title: "The Pinkprint",
+		author: "Nicki Minaj",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+		picture: "https://rovimusic.rovicorp.com/image.jpg?c=LV2Lx-FFKENONqcF31s8MoAf7E_1E-2MlBBPmPAXRBU=&f=5",
+		songs: [{
+				pos: 1,
+				title: "All Things Go",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 2,
+				title: "I Lied",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 3,
+				title: "The Crying Game",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 4,
+				title: "Get on Your Knees",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 5,
+				title: "Feeling Myself",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 6,
+				title: "Only",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 7,
+				title: "Want Some More",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 8,
+				title: "Four Door Aventador",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 9,
+				title: "Favorite",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: "10",
+				title: "Buy a Heart",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: "11",
+				title: "Trini Dem Girls",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: "12",
+				title: "Anaconda",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: "13",
+				title: "The Night Is Still Young",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: "14",
+				title: "Pills N Potions",
+				file: "http://music.com/song/5678/file.mp3"
+			}
+		]
+	},{
+		id: 2,
+		title: "Broken Silence",
+		author: "Foxy Brown",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+		picture: "https://rovimusic.rovicorp.com/image.jpg?c=gBXNqHEhIUQC8oD9JlIn0phUoDg0hsvx4F4sL4oO-nA=&f=5",
+		songs: [{
+				pos: 1,
+				title: "Intro - Broken Silence",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 2,
+				title: "Fallin'",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 3,
+				title: "Oh Yeah",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 4,
+				title: "B.K. Anthem",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 5,
+				title: "The Letter",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 6,
+				title: "730",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 7,
+				title: "Candy",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 8,
+				title: "Tables Will Turn",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 9,
+				title: "Hood Scriptures",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 10,
+				title: "Run Dem",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 11,
+				title: "'Bout My Paper",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 12,
+				title: " Run Yo S**",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 13,
+				title: "Na Na Be Like",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 14,
+				title: "Gangsta Boogie",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 15,
+				title: "I Don't Care",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 16,
+				title: "So Hot",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 17,
+				title: "Saddest Day",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 18,
+				title: "Broken Silence",
+				file: "http://music.com/song/5678/file.mp3"
+			}
+		]
+	},{
+		id: 3,
+		title: "My Spanish Heart",
+		author: "Chick Corea",
+		desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+		picture: "https://rovimusic.rovicorp.com/image.jpg?c=P_CBSQLmzUgMLXOhdhCTSRyhM-OFI8zG4l-qVpXXB1I=&f=5",
+		songs: [{
+				pos: 1,
+				title: "Love Castle",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 2,
+				title: "The Gardens",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 3,
+				title: "Day Danse",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 4,
+				title: "My Spanish Heart",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 5,
+				title: "Night Streets",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 6,
+				title: "The Hilltop",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 7,
+				title: "Wind Danse",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 8,
+				title: "Armando's Rhumba",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 9,
+				title: "Prelude to el Bozo",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 10,
+				title: "El Bozo, Pt. 1",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 11,
+				title: "El Bozo, Pt. 2",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 12,
+				title: "El Bozo, Pt. 3",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 13,
+				title: "Spanish Fantasy, Pt. 1",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 14,
+				title: "Spanish Fantasy, Pt. 2",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 15,
+				title: "Spanish Fantasy, Pt. 3",
+				file: "http://music.com/song/5678/file.mp3"
+			},{
+				pos: 16,
+				title: "Spanish Fantasy, Pt. 4",
+				file: "http://music.com/song/5678/file.mp3"
+			}
+		]
+	}
+];
+
